2f3dc60760fb978954ca3a0d03055a67bcc10ca0
Phas3r <sergeisl95@yandex.ru>
2014-10-10 00:38:39 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,125 +1,183 @@
 #include <iostream>
 #include <queue>
-#include <set>
 #include <vector>
 #include <ctime>
-
+#include <iterator>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <set>
+#include <algorithm>
 using namespace std;
 
-struct Marks {
-  bool visited;
-  bool topmark;
-  bool bottommark;
-  bool inL;
-  bool inK;
-  Marks(bool a = 0, bool b = 0, bool c = 0, bool d = 0, bool e = 0)
-      : visited(a), topmark(b), bottommark(c), inL(d), inK(e) {}
-};
+struct Marks 
+	{
+	bool visited;
+	bool topmark;
+	bool bottommark;
+	bool inL;
+	bool inK;
+	Marks(bool a = 0, bool b = 0, bool c = 0, bool d = 0, bool e = 0)
+		: visited(a), topmark(b), bottommark(c), inL(d), inK(e) {}
+	};
 
-int main() {
-  int l, t;
-  int k, n;
-  cout << "Enter number of nodes in graph";
-  cin >> t;
-  queue<int> schedule;
-  queue<int> from;  // 1 -from child, 2 - from both, 3 - from parent
-  vector<vector<int>> graph(t,
-                            vector<int>(t));  // строка - откуда, столбец - куда
-  srand((unsigned)time(NULL));
-  for (int i = 0; i < t; i++)
-    for (int j = 0; j < t; j++) {
-      if (i == j) {
-        graph[i][j] = 0;
-      } else {
-        graph[i][j] = (rand_r() % 2);
-        graph[j][i] = 0;
-      }
-    }
-  for (int i = 0; i < t; i++) {
-    for (int j = 0; j < t; j++) {
-      cout << graph[i][j] << ' ';
-    }
-    cout << endl;
-  }
+void getmatrix(fstream& Matrix,vector<vector<int> >& graph)
+	{
 
-  vector<Marks> marks(t);
 
-  for (int i = 0; i < t; i++) {
-    Marks temp(0);
-    marks[i] = temp;
-  }
+	if (Matrix)
+		{
+		string line;
+		while (getline(Matrix, line)) 
+			{
+			istringstream iss(line);   
+			graph.push_back(vector<int>(istream_iterator<int>(iss), istream_iterator<int>()));
+			}
+		for (int i=0;i<graph[0].size();++i)
+			{
+			for (int j = 0; j < graph[0].size(); ++j)
+				{
+				cout<<graph[i][j]<<' ';
+				}
+			printf("\n");
+			}
+		}
+	else
+		{
+		cout<<" Wrong filename"<<endl;
+		system("Pause");
+		exit(1);
 
-  cout << "Enter number of nodes in L:" << endl;
-  cin >> l;
-  cout << "Enter set L:" << endl;
-  for (int i = 0; i < l; i++) {
-    cin >> t;
-    marks[t - 1].inL = true;
-    schedule.push(t);
-    from.push(1);
-  }
-  cout << "Enter number of nodes in K:" << endl;
-  cin >> k;
-  cout << "Enter set K:" << endl;
-  for (int i = 0; i < k; i++) {
-    cin >> t;
-    marks[t - 1].inK = true;
-  }
-  while (!schedule.empty()) {
-    t = schedule.front();
-    schedule.pop();
-    if (marks[t].visited) continue;
-    marks[t].visited = true;
-    n = from.front();
-    from.pop();
-    if ((n < 3) && (!marks[t].inK)) {  // (c)
-      if (!marks[t].topmark) {  // i
-        marks[t].topmark = true;
-        for (int i = 0; i < graph[0].size(); i++) {
-          if (graph[i][t] > 0) {
-            schedule.push(i);
-            from.push(1);
-          }
-        }
-      }
-      if (!marks[t].bottommark) {  // ii
-        marks[t].bottommark = true;
-        for (int i = 0; i < graph[0].size(); i++) {
-          if (graph[t][i] > 0) {
-            schedule.push(i);
-            from.push(2);
-          }
-        }
-      }
-    }
-    if (n > 1) {  // (d)
-      if ((marks[t].inK) && (!marks[t].topmark)) {  // i
-        marks[t].topmark = true;
-        for (int i = 0; i < graph[0].size(); i++) {
-          if (graph[i][t] > 0) {
-            schedule.push(i);
-            from.push(1);
-          }
-        }
-      }
-      if ((!marks[t].inK) && (!marks[t].bottommark)) {  // ii
-        marks[t].bottommark = true;
-        for (int i = 0; i < graph[0].size(); i++) {
-          if (graph[t][i] > 0) {
-            schedule.push(i);
-            from.push(2);
-          }
-        }
-      }
-    }
-  }
+		}
+	}
 
-  cout << 'L' << static_cast<char>(193) << 'J'
-       << static_cast<char>(179) << 'K' << endl;
-  cout << "J:" << endl;
-  for (int i = 0; i < marks.size(); i++) {
-    if (!marks[i].bottommark) cout << i << endl;
-  }
-  system("Pause");
-  return 0;
-}
+void Ball(vector<vector<int> >& graph, set<int>& a, const set<int> L, const set<int> K)
+	{
+	queue<int> schedule;
+	queue<int> from;  // 1 -from child, 2 - from parent
+	vector<Marks> marks(graph[0].size());
+	for (int i = 0; i < marks.size(); i++) 
+		{
+		Marks temp(0);
+		marks[i] = temp;
+		}
+	set<int>::iterator iter;
+	for (iter=L.begin();iter!=L.end();++iter)
+		{
+		marks[*(iter)-1].inL = true;
+		schedule.push(*(iter));
+		from.push(1);
+		}  
+	for (iter=K.begin();iter!=K.end();++iter)
+		{
+		marks[*(iter) - 1].inK = true;
+		}
+
+
+	int t,n;
+	while (!schedule.empty())
+		{
+		t = (schedule.front()-1);
+		schedule.pop();
+		marks[t].visited = true;
+		n = from.front();
+		from.pop();
+		if ((n ==1) && (!marks[t].inK))  // (c)
+			{ 
+			if (!marks[t].topmark) // i
+				{  
+				marks[t].topmark = true;
+				for (int i = 0; i < graph[0].size(); i++) 
+					{
+					if (graph[i][t] > 0) 
+						{
+						schedule.push(i+1);
+						from.push(1);
+						}
+					}
+				}
+			if (!marks[t].bottommark)
+				{  // ii
+				marks[t].bottommark = true;
+				for (int i = 0; i < graph[0].size(); i++)
+					{
+					if (graph[t][i] > 0)
+						{
+						schedule.push(i+1);
+						from.push(2);
+						}
+					}
+				}
+			}
+		if (n == 2) 
+			{  // (d)
+			if ((marks[t].inK) && (!marks[t].topmark))
+				{  // i
+				marks[t].topmark = true;
+				for (int i = 0; i < graph[0].size(); i++) 
+					{
+					if (graph[i][t] > 0) 
+						{
+						schedule.push(i+1);
+						from.push(1);
+						}
+					}
+				}
+			if ((!marks[t].inK) && (!marks[t].bottommark))
+				{  // ii
+				marks[t].bottommark = true;
+				for (int i = 0; i < graph[0].size(); ++i) 
+					{
+					if (graph[t][i] > 0)
+						{
+						schedule.push(i+1);
+						from.push(2);
+						}
+					}
+				}
+			}
+		}
+	for (int i = 0; i < marks.size(); ++i)
+		{
+		if (!marks[i].bottommark) a.insert(i+1);
+		}
+	}
+int main() 
+	{
+	int l, t;
+	int k, n;
+	vector<vector<int> > graph;
+	set<int> L,J,K;
+	char filename[80];
+	cout<<"Enter path: \n";
+	cin>>filename;
+	fstream ifst(filename);
+	getmatrix(ifst,graph);
+	cout << "Enter number of nodes in L:" << endl;
+	cin >> l;
+	cout << "Enter set L:" << endl;
+	for (int i = 0; i < l; i++) 
+		{
+		cin >> t;
+		L.insert(t);
+		}
+	cout << "Enter number of nodes in K:" << endl;
+	cin >> k;
+	cout << "Enter set K:" << endl;
+	for (int i = 0; i < k; i++)
+		{
+		cin >> t;
+		K.insert(t);
+		}
+	Ball(graph,J,L,K);
+
+	cout << 'J' << static_cast<char>(193) << 'L'
+		<< static_cast<char>(179) << 'K' << endl;
+	cout << "J:" << endl;
+	set<int>::iterator ik;
+	for(ik=J.begin(); ik!=J.end(); ++ik)
+		cout<<*ik<<"  ";
+
+	system("Pause");
+	return 0;
+	}
